53b1d85c3437c2f2ad743e0d7a174afcdad8b078
Muhamad Sidiq <muhammadsidiq.nr@gmail.com>
2026-09-16 21:11:40 +0800

    style(frontend): terapkan sidebar admin di halaman wilayah rawan

--- resources/views/admin/wilayah/index.blade.php
+++ resources/views/admin/wilayah/index.blade.php
@@ -1,5 +1,10 @@
-<x-app-layout>
+@extends('layouts.admin')
+
+@section('header', 'Kelola Wilayah Rawan Bencana')
+
+@section('content')
     <div class="p-6 max-w-5xl mx-auto">
+
         <div class="flex justify-between items-center mb-4">
             <h1 class="text-xl font-bold">Kelola Wilayah Rawan Bencana</h1>
             <a href="{{ route('admin.wilayah.create') }}" class="bg-blue-600 text-white px-4 py-2 rounded text-sm">
@@ -68,4 +73,4 @@
 
         {{ $wilayah->links() }}
     </div>
-</x-app-layout>
+@endsection
