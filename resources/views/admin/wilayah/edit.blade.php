5ed3e2f675755f6c2afd660b6234a19c8e4c2f1a
Muhamad Sidiq <muhammadsidiq.nr@gmail.com>
2026-09-17 17:21:09 +0800

    style(frontend): terapkan sidebar admin di halaman edit wilayah rawan

--- resources/views/admin/wilayah/edit.blade.php
+++ resources/views/admin/wilayah/edit.blade.php
@@ -1,4 +1,8 @@
-<x-app-layout>
+@extends('layouts.admin')
+
+@section('header', 'Edit Wilayah Rawan Bencana')
+
+@section('content')
     <div class="p-6 max-w-xl mx-auto">
         <h1 class="text-xl font-bold mb-4">Edit Wilayah Rawan Bencana</h1>
 
@@ -44,4 +48,4 @@
             <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded">Update</button>
         </form>
     </div>
-</x-app-layout>
+@endsection
