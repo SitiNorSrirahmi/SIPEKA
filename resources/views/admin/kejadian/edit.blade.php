73ec591144d1bc8872f85f9aa3cb85f50f87bbdd
Muhamad Sidiq <muhammadsidiq.nr@gmail.com>
2026-09-17 16:44:21 +0800

    style(frontend): terapkan sidebar admin di halaman edit kejadian

--- resources/views/admin/kejadian/edit.blade.php
+++ resources/views/admin/kejadian/edit.blade.php
@@ -1,4 +1,8 @@
-<x-app-layout>
+@extends('layouts.admin')
+
+@section('header', 'Edit Data Kejadian Bencana')
+
+@section('content')
     <div class="p-6 max-w-xl mx-auto">
         <h1 class="text-xl font-bold mb-4">Edit Data Kejadian Bencana</h1>
 
@@ -43,4 +47,4 @@
             <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded">Update</button>
         </form>
     </div>
-</x-app-layout>
+@endsection
